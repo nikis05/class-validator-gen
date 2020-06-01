05e1f72c31bc149d40072d3ae09ee9ade1ff8eb4
Nikita Isaev <nikis05@mail.ru>
2020-06-01 21:31:06 +0300

    V0.2 Add support for validation options + emit ts instead of json

--- examples/User.schema.ts
+++ examples/User.schema.ts
@@ -1,28 +1,36 @@
-import { IsDefined, MinLength, IsDate as CVIsDate /* aliases work ok */ } from 'class-validator';
+import {
+  IsDefined,
+  MinLength,
+  IsDate as CVIsDate /* aliases work ok */,
+} from 'class-validator';
 // Decorators that are not imported from CV are discarded
 import IsSomething from 'some-module';
 
 // Decorators that are not imported from CV are discarded
-const GarbageDecorator = (): PropertyDecorator => (_target: any, _propertyKey: any) => { };
+const GarbageDecorator = (): PropertyDecorator => (
+  _target: any,
+  _propertyKey: any
+) => {};
 /*
- * Class-validator-gen cannot figure out that this decorator is coming from CV, 
+ * Class-validator-gen cannot figure out that this decorator is coming from CV,
  * hence it is ignored
  */
 const MyDecorator = IsDefined;
 
 export class User {
-    @GarbageDecorator()
-    @IsDefined()
-    @MinLength(10, '20', /30/)
-    name!: string;
+  @GarbageDecorator()
+  @IsDefined({ message: 'Custom error message' })
+  @IsNumberString({})
+  @MinLength(10, '20', /30/)
+  name!: string;
 
-    @CVIsDate()
-    dateOfBirth!: string;
+  @CVIsDate()
+  dateOfBirth!: string;
 
-    @IsSomething()
-    @MyDecorator()
-    status!: string;
+  @IsSomething()
+  @MyDecorator()
+  status!: string;
 }
 
 // This class does not have CV decorators, hence it is discarded
-export class Garbage { }
+export class Garbage {}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,24 @@ import * as ts from 'typescript';
 import glob from 'glob';
 import path from 'path';
 import fs from 'fs';
-import { ValidationSchema } from 'class-validator';
+
+interface ValidationSchema {
+  name: string;
+  properties: { [key: string]: ValidatorConfig[] };
+}
+
+interface ValidatorConfig {
+  type: string;
+  constraints: ts.Expression[];
+  message?: ts.StringLiteral | ts.ArrowFunction;
+  each?: ts.BooleanLiteral;
+  always?: ts.BooleanLiteral;
+  groups?: ts.ArrayLiteralExpression;
+}
+
+const OPTION_NAMES = ['message', 'each', 'always', 'groups'];
+
+type ValidatorOptions = Omit<ValidatorConfig, 'type' | 'constraints'>;
 
 const base_dir = process.argv[2];
 const source_glob = process.argv[3];
@@ -13,180 +30,300 @@ const dist = path.join(process.cwd(), dist_dir);
 
 // Check if dist directory exist
 if (!fs.existsSync(dist)) {
-    // Create directory path
-    fs.mkdirSync(dist, { recursive: true });
+  // Create directory path
+  fs.mkdirSync(dist, { recursive: true });
 }
 
-const getFileNames = (): string[] => glob.sync(source_glob, { cwd: base, absolute: true });
+const getFileNames = (): string[] =>
+  glob.sync(source_glob, { cwd: base, absolute: true });
 
 interface File {
-    name: string;
-    content: string;
+  name: string;
+  content: string;
 }
 
 const loadFile = (fileName: string): File => ({
-    name: fileName,
-    content: fs.readFileSync(fileName).toString()
+  name: fileName,
+  content: fs.readFileSync(fileName).toString(),
 });
 
-const createSourceFile = (file: File): ts.SourceFile => ts.createSourceFile(
-    file.name,
-    file.content,
-    ts.ScriptTarget.ESNext
-);
+const createSourceFile = (file: File): ts.SourceFile =>
+  ts.createSourceFile(file.name, file.content, ts.ScriptTarget.ESNext);
 
 type CVDecoratorNames = Array<{
-    name: string;
-    alias: string;
-}>
+  name: string;
+  alias: string;
+}>;
 
-const extractCVDecoratorNames = (importDeclarations: ts.ImportDeclaration[]): CVDecoratorNames => {
-    const cvImportDeclarations = importDeclarations
-        .filter(importDeclaration =>
-            (importDeclaration.moduleSpecifier as ts.StringLiteral).text === 'class-validator'
-        );
+const extractCVDecoratorNames = (
+  importDeclarations: ts.ImportDeclaration[]
+): CVDecoratorNames => {
+  const cvImportDeclarations = importDeclarations.filter(
+    (importDeclaration) =>
+      (importDeclaration.moduleSpecifier as ts.StringLiteral).text ===
+      'class-validator'
+  );
 
-    const cvDecoratorNames = cvImportDeclarations.reduce<CVDecoratorNames>((acc, importDeclaration) => {
-        const importClause = importDeclaration.importClause;
-        if (!importClause) return acc;
-        const namedBindings = importClause.namedBindings;
-        if (!namedBindings || namedBindings.kind !== ts.SyntaxKind.NamedImports) return acc;
+  const cvDecoratorNames = cvImportDeclarations.reduce<CVDecoratorNames>(
+    (acc, importDeclaration) => {
+      const importClause = importDeclaration.importClause;
+      if (!importClause) return acc;
+      const namedBindings = importClause.namedBindings;
+      if (!namedBindings || namedBindings.kind !== ts.SyntaxKind.NamedImports)
+        return acc;
 
-        const decoratorNames = namedBindings.elements
-            .map(importSpecifier => {
-                const alias = importSpecifier.name.text;
-                const name = (
-                    importSpecifier.propertyName
-                    && importSpecifier.propertyName.text
-                ) || importSpecifier.name.text;
-                return { name, alias };
-            });
-        return [...acc, ...decoratorNames];
-    }, []);
+      const decoratorNames = namedBindings.elements.map((importSpecifier) => {
+        const alias = importSpecifier.name.text;
+        const name =
+          (importSpecifier.propertyName && importSpecifier.propertyName.text) ||
+          importSpecifier.name.text;
+        return { name, alias };
+      });
+      return [...acc, ...decoratorNames];
+    },
+    []
+  );
 
-    return cvDecoratorNames;
-}
+  return cvDecoratorNames;
+};
 
-const isCallExpression = (
-    expression: ts.LeftHandSideExpression
-): expression is ts.CallExpression => {
-    return expression.kind === ts.SyntaxKind.CallExpression;
-}
+const decapitalize = (str: string) =>
+  str.charAt(0).toLowerCase() + str.substr(1);
 
-const isIdentifier = (
-    expression: ts.LeftHandSideExpression
-): expression is ts.Identifier => {
-    return expression.kind === ts.SyntaxKind.Identifier;
-}
+const isOptionsArg = (
+  expression: ts.Expression
+): expression is ts.ObjectLiteralExpression => {
+  if (!ts.isObjectLiteralExpression(expression)) return false;
+  return expression.properties.every((property) => {
+    const name = property.name;
+    if (!name || !ts.isIdentifier(name)) return false;
+    return OPTION_NAMES.includes(name.text);
+  });
+};
 
-const decapitalize = (str: string) => str.charAt(0).toLowerCase() + str.substr(1);
+const extractValidatorOptionsFromOptionsArg = (
+  optionsArg: ts.ObjectLiteralExpression
+): ValidatorOptions => {
+  return optionsArg.properties.reduce<ValidatorOptions>((acc, property) => {
+    const name = property.name;
+    if (!name || !ts.isIdentifier(name))
+      throw new Error('Expected option property name to be indentifier');
+
+    if (!ts.isPropertyAssignment(property))
+      throw new Error('Expected option property to be property assignment');
+
+    const initializer = property.initializer;
+    if (
+      name.text === 'message' &&
+      !ts.isStringLiteral(initializer) &&
+      !ts.isArrowFunction(initializer)
+    )
+      throw new Error(
+        'Expected message to be string literal or arrow function'
+      );
+
+    if (
+      name.text === 'each' &&
+      initializer.kind !== ts.SyntaxKind.TrueKeyword &&
+      initializer.kind !== ts.SyntaxKind.FalseKeyword
+    )
+      throw new Error('Expected each to be boolean literal');
+
+    if (
+      name.text === 'always' &&
+      initializer.kind !== ts.SyntaxKind.TrueKeyword &&
+      initializer.kind !== ts.SyntaxKind.FalseKeyword
+    )
+      throw new Error('Expected always to be boolean literal');
+
+    if (name.text === 'groups') {
+      if (!ts.isArrayLiteralExpression(initializer))
+        throw new Error('Expected groups to be array literal');
+      initializer.elements.forEach((element) => {
+        if (!ts.isStringLiteral(element))
+          throw new Error('Expected elements of groups to be string literals');
+      });
+    }
+
+    return { ...acc, [name.text]: initializer };
+  }, {});
+};
+
+const validateConstraints = (args: ts.Expression[]) => {
+  args.forEach((arg) => {
+    if (
+      !ts.isStringLiteral(arg) &&
+      !ts.isObjectLiteralExpression(arg) &&
+      !ts.isRegularExpressionLiteral(arg) &&
+      !ts.isNumericLiteral(arg)
+    )
+      throw new Error(`Expected constraint to be a string literal,
+        an object literal expression, a regular expression or a numeric literal`);
+  });
+};
 
 const extractValidatorsFromClassElement = (
-    cvDecoratorNames: CVDecoratorNames,
-    classElement: ts.ClassElement
-): Array<{ type: string, constraints: any[] }> => {
-    if (!classElement.decorators) return [];
-    return classElement.decorators
-        .reduce<Array<{ type: string, constraints: any[] }>>((acc, decorator) => {
-            const callExpression = decorator.expression;
-            if (!isCallExpression(callExpression)) return acc;
+  cvDecoratorNames: CVDecoratorNames,
+  classElement: ts.ClassElement
+): ValidatorConfig[] => {
+  if (!classElement.decorators) return [];
+  return classElement.decorators.reduce<ValidatorConfig[]>((acc, decorator) => {
+    const callExpression = decorator.expression;
+    if (!ts.isCallExpression(callExpression)) return acc;
 
-            const identifier = callExpression.expression
-            if (!isIdentifier(identifier)) return acc;
+    const identifier = callExpression.expression;
+    if (!ts.isIdentifier(identifier)) return acc;
 
-            const decoratorAlias = identifier.text;
-            const cvDecoratorName = cvDecoratorNames.find(cvDecoratorName =>
-                cvDecoratorName.alias === decoratorAlias
-            );
-            if (!cvDecoratorName) return acc;
+    const decoratorAlias = identifier.text;
+    const cvDecoratorName = cvDecoratorNames.find(
+      (cvDecoratorName) => cvDecoratorName.alias === decoratorAlias
+    );
+    if (!cvDecoratorName) return acc;
 
-            const constraints = callExpression.arguments.map(argument => {
-                if (
-                    argument.kind !== ts.SyntaxKind.StringLiteral
-                    && argument.kind !== ts.SyntaxKind.NumericLiteral
-                    && argument.kind !== ts.SyntaxKind.RegularExpressionLiteral
-                ) throw new Error(
-                    `Unsupported syntax kind in ClassValidator decorator:
-                             Expected string literal, numeric literal or regex literal.`
-                );
-                return (argument as ts.StringLiteral).text;
-            });
+    let args = [...callExpression.arguments];
 
-            return [...acc, { type: decapitalize(cvDecoratorName.name), constraints }];
-        }, [])
-}
+    const lastArg = args[args.length - 1];
+    const optionsArg = lastArg && isOptionsArg(lastArg) ? lastArg : null;
+
+    if (optionsArg) args = args.slice(0, args.length - 1);
+
+    const options = optionsArg
+      ? extractValidatorOptionsFromOptionsArg(optionsArg)
+      : null;
+
+    validateConstraints(args);
+
+    return [
+      ...acc,
+      {
+        type: decapitalize(cvDecoratorName.name),
+        constraints: args,
+        ...options,
+      },
+    ];
+  }, []);
+};
 
 const extractCVSchemaFromClass = (
-    cvDecoratorNames: CVDecoratorNames,
-    classDeclaration: ts.ClassDeclaration
+  cvDecoratorNames: CVDecoratorNames,
+  classDeclaration: ts.ClassDeclaration
 ): ValidationSchema | null => {
-    const properties = classDeclaration.members
-        .reduce<ValidationSchema["properties"]>((acc, classElement) => {
-            if (!classElement.name || classElement.name.kind !== ts.SyntaxKind.Identifier) return acc;
-            const validators = extractValidatorsFromClassElement(cvDecoratorNames, classElement);
-            if (validators.length === 0) return acc;
-            return { ...acc, [classElement.name.text]: validators };
-        }, {});
+  const properties = classDeclaration.members.reduce<
+    ValidationSchema['properties']
+  >((acc, classElement) => {
+    if (
+      !classElement.name ||
+      classElement.name.kind !== ts.SyntaxKind.Identifier
+    )
+      return acc;
+    const validators = extractValidatorsFromClassElement(
+      cvDecoratorNames,
+      classElement
+    );
+    if (validators.length === 0) return acc;
+    return { ...acc, [classElement.name.text]: validators };
+  }, {});
 
-    if (Object.keys(properties).length === 0) return null;
+  if (Object.keys(properties).length === 0) return null;
 
-    if (!classDeclaration.name) return null;
-    return { name: classDeclaration.name.text, properties };
-}
+  if (!classDeclaration.name)
+    throw new Error('ClassValidator class declaration must be named');
+  return { name: classDeclaration.name.text, properties };
+};
 
 const processSourceFile = (sourceFile: ts.SourceFile) => {
-    const importDeclarations = sourceFile.statements
-        .filter((statement): statement is ts.ImportDeclaration =>
-            statement.kind === ts.SyntaxKind.ImportDeclaration
-        );
+  const importDeclarations = sourceFile.statements.filter(
+    ts.isImportDeclaration
+  );
 
-    const cvDecoratorNames = extractCVDecoratorNames(importDeclarations);
+  const cvDecoratorNames = extractCVDecoratorNames(importDeclarations);
 
-    const classDeclarations = sourceFile.statements
-        .filter((statement): statement is ts.ClassDeclaration =>
-            statement.kind === ts.SyntaxKind.ClassDeclaration
-        );
+  const classDeclarations = sourceFile.statements.filter(
+    (statement): statement is ts.ClassDeclaration =>
+      statement.kind === ts.SyntaxKind.ClassDeclaration
+  );
 
-    const validationSchemas = classDeclarations
-        .map(classDeclaration => extractCVSchemaFromClass(cvDecoratorNames, classDeclaration))
-        .filter((schema): schema is ValidationSchema => schema !== null);
+  const validationSchemas = classDeclarations
+    .map((classDeclaration) =>
+      extractCVSchemaFromClass(cvDecoratorNames, classDeclaration)
+    )
+    .filter((schema): schema is ValidationSchema => schema !== null);
 
-    return validationSchemas;
-}
+  return validationSchemas;
+};
 
-const emitSchemaFile = (validationShema: ValidationSchema): File => {
-    return {
-        name: validationShema.name + '.schema.json',
-        content: JSON.stringify(validationShema)
-    }
-}
+const emitSchemasFile = (validationShemas: ValidationSchema[]): File => {
+  const sourceFile = ts.createSourceFile(
+    'registerSchemas.ts',
+    '',
+    ts.ScriptTarget.ESNext
+  );
+  const printer = ts.createPrinter();
+  const emitExpression = (node: ts.Node) =>
+    printer.printNode(ts.EmitHint.Expression, node, sourceFile);
 
-const emitHeaderFile = (validationShemas: ValidationSchema[]): File => {
-    return {
-        name: 'registerSchemas.ts',
-        content: [
-            "import { registerSchema } from 'class-validator'",
-            validationShemas.map(validationShema =>
-                `registerSchema(require("./${validationShema.name}.schema.json"))`
-            )
-        ].join("\n")
-    }
-}
+  return {
+    name: 'registerSchemas.ts',
+    content: [
+      "import { registerSchema } from 'class-validator'",
+      validationShemas.map(
+        (validationShema) =>
+          `registerSchema({
+            name: "${validationShema.name}",
+            properties: {
+              ${Object.entries(validationShema.properties).map(
+                ([property, validators]) => `
+                ${property}: [
+                  ${validators.map(
+                    (validator) => `
+                    {
+                      type: "${validator.type}",
+                      constraints: [${validator.constraints
+                        .map(emitExpression)
+                        .join(',')}],
+                      message: ${
+                        validator.message
+                          ? emitExpression(validator.message)
+                          : 'undefined'
+                      },
+                      each: ${
+                        validator.each
+                          ? emitExpression(validator.each)
+                          : 'undefined'
+                      },
+                      always: ${
+                        validator.always
+                          ? emitExpression(validator.always)
+                          : 'undefined'
+                      },
+                      groups: ${
+                        validator.groups
+                          ? emitExpression(validator.groups)
+                          : '[]'
+                      }
+                    },
+                  `
+                  )}
+                ]
+              `
+              )}
+            }
+          })`
+      ),
+    ].join('\n'),
+  };
+};
 
 const writeFileToDisc = (file: File) => {
-    fs.writeFileSync(path.join(dist, file.name), file.content, { flag: 'w' });
-}
+  fs.writeFileSync(path.join(dist, file.name), file.content, { flag: 'w' });
+};
 
 const validationShemas = getFileNames()
-    .map(loadFile)
-    .map(createSourceFile)
-    .reduce<ValidationSchema[]>((acc, sourceFile) =>
-        [...acc, ...processSourceFile(sourceFile)], []
-    );
+  .map(loadFile)
+  .map(createSourceFile)
+  .reduce<ValidationSchema[]>(
+    (acc, sourceFile) => [...acc, ...processSourceFile(sourceFile)],
+    []
+  );
 
-const outputFiles = [
-    ...validationShemas.map(validationShema => emitSchemaFile(validationShema)),
-    emitHeaderFile(validationShemas)
-];
-
-outputFiles.forEach(file => writeFileToDisc(file));
+writeFileToDisc(emitSchemasFile(validationShemas));
